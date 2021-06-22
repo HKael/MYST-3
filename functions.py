02145c365907cb246bb4af4c2dde149a03172eeb
Alejandro Michel <alejandromichelmarron@gmail.com>
2021-06-21 22:53:14 -0500

    Estadistica Descriptiva Done

--- functions.py
+++ functions.py
@@ -105,7 +105,7 @@ def f_columnas_tiempos(param_data):
 
 
 # Function to add pip columns
-def f_columnas_pips():
+def f_columnas_pips(param_data):
     """
      Name:
      -------
@@ -136,11 +136,18 @@ def f_columnas_pips():
         profit_acm: The accumulated value of the profit column
 
      """
-    return
+    rows = []
+    for x in range(len(param_data)):
+        rows.append(float(f_pip_size(param_data.loc[x, "Symbol"])))
+    provdf = pd.DataFrame(rows, columns=["Pips"])
+    param_data["Pips"] = (param_data["Close Price"] - param_data["Open Price"])*(provdf['Pips'])
+    param_data["Pips Accumulated"] = param_data["Pips"].cumsum()
+    param_data["Profit Accumulated"] = param_data["Profit"].cumsum()
+    return param_data
 
 
 # Function to calculate basic statistics parameters
-def f_estadisticas_ba():
+def f_estadisticas_ba(param_data):
     """
     Name:
     -------
@@ -163,7 +170,78 @@ def f_estadisticas_ba():
         Gets a dictionary with the keys 'df_1_table' and 'df_2_ranking'.
 
      """
-    return
+
+    def f_estadisticas_ba(param_data):
+
+        # DataFrame 1
+        buy_count, sell_count, pos_count_sell, neg_count_sell, pos_count_buy, neg_count_buy = 0, 0, 0, 0, 0, 0
+        profit_median = param_data["Profit"].median()
+        pip_median = param_data["Pips"].median()
+        # Number of operations
+        total_count = (len(param_data))
+        for x in range(total_count):
+            # Count buy and sell operations
+            if param_data.loc[x, "Type"] == "buy":
+                buy_count += 1
+                # Count positve and negative operations
+                if param_data.loc[x, "Profit"] >= 0:
+                    pos_count_buy += 1
+
+                else:
+                    neg_count_buy += 1
+            else:
+                sell_count += 1
+                # Count positve and negative operations
+                if param_data.loc[x, "Profit"] >= 0:
+                    pos_count_sell += 1
+
+                else:
+                    neg_count_sell += 1
+
+        r_efectividad = (pos_count_buy + pos_count_sell) / total_count
+        r_proporcion = (pos_count_buy + pos_count_sell) / (neg_count_buy + neg_count_sell)
+        r_efectividad_c = pos_count_buy / total_count
+        r_efectividad_v = pos_count_sell / total_count
+
+        medida = ["Ops Totales", "Ganadoras", "Ganadoras_c", "Ganadoras_v", "Perdedoras", "Perdedoras_c",
+                  "Perdedoras_v", "Mediana(Profit)", "Mediana(Pips)", "r_efectividad", "r_proporcion",
+                  "r_efectividad_c", "r_efectividad_v"]
+
+        values = np.array([total_count, (pos_count_buy + pos_count_sell), pos_count_buy, pos_count_sell,
+                           (neg_count_buy + neg_count_sell), neg_count_buy, neg_count_sell, profit_median,
+                           pip_median, r_efectividad, r_proporcion, r_efectividad_c, r_efectividad_v])
+
+        description = ["Operaciones Totales", "Operaciones Ganadoras", "Operaciones Ganadoras de Compra",
+                       "Operaciones Ganadoras de Venta", "Operaciones Perdedoras", "Operaciones Perdedoras de Compra",
+                       "Operaciones Perdedoras de Venta", "Mediana de Profit de Operaciones",
+                       "Mediana de Pips de Operaciones", "Ganadoras Totales/Operaciones Totales",
+                       "Ganadoras Totales/Perdedoras Totales", "Ganadoras Compras/Operaciones Totales",
+                       "Ganadoras Ventas/Operaciones Totales"]
+
+        df1 = pd.DataFrame({'Measurment': medida, 'Value': values, 'Description': description})
+
+        # DatFrame 2
+        symbols = np.unique(param_data['Symbol'])  # listo ya tenemos los unicos
+        rank = []
+        for i in range(len(symbols)):
+            pos_count_rank = 0
+            neg_count_rank = 0
+            for j in range(len(param_data)):
+                if symbols[i] == param_data['Symbol'].iloc[j] and param_data['Profit'].iloc[j] > 0:
+                    pos_count_rank += 1
+
+                elif symbols[i] == param_data['Symbol'].iloc[j] and param_data['Profit'].iloc[j] < 0:
+                    neg_count_rank += 1
+
+            total = neg_count_rank + pos_count_rank
+            rank.append(pos_count_rank / total)
+
+        df2 = pd.DataFrame({'Symbol': symbols, 'Rank': rank})
+        df2 = df2.sort_values(by='rank', ascending=False)
+
+        # Combine both to make the dictionary
+        dictionary = {'df_1_tabla': df1, 'df_2_ranking': df2}
+        return dictionary
 
 # %% Performance Attribution Measures
 
